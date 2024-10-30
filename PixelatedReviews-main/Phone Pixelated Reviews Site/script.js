48df1dbbef5807bb3783fc3e283f78a645e6b885
EllyBard <EliShopbell@gmail.com>
2024-10-30 14:06:19 -0500

    Even more changes to mobile
    yipee

--- PixelatedReviews-main/Phone Pixelated Reviews Site/script.js
+++ PixelatedReviews-main/Phone Pixelated Reviews Site/script.js
@@ -1,10 +1,10 @@
 function toggleVisibility() {
     const passwordInput = document.getElementById('password');
-    const toggleIcon = document.getElementById('toggle-icon');
+    toggleIcon = document.getElementById('toggle-icon');
     
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
-        toggleIcon.src = 'Visability.png'; // Change to visibility-on image
+        toggleIcon.src = 'Visibility.png'; // Change to visibility-on image
     } else {
         passwordInput.type = 'password';
         toggleIcon.src = 'VisibilityOff.png'; // Change to visibility-off image
